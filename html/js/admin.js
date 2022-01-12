bcafd7b516ea35f77ebbf2bf3e016066db544041
Shah Harsh <harshshah04042001@gmail.com>
2022-01-12 18:30:45 +0530

    changes according to srs

--- html/js/admin.js
+++ html/js/admin.js
@@ -10,17 +10,81 @@ $(function () {
     $('#datetimepicker3').datepicker();
 });
 
-// $('.menu_option_dot').click(function () { 
-//     $('.menu_option_dot').toggleClass('menu_option_click');  
-// });
-
 $('.edit_reschedule').click(function () { 
     $('.bg-modal').css('display', 'flex');
 });
 
 $('.modal_close').click(function () { 
     $('.bg-modal').css('display', 'none');
+    $('.bg-modal-fp').css('display', 'none');
 });
+
+$('#login').click(function () { 
+    $('.bg-modal').css('display', 'flex');
+});
+
+$('#login_fp').click(function () { 
+    $('.bg-modal').css('display', 'flex');
+    $('.bg-modal-fp').css('display', 'none');
+});
+
+$('#forgot_password').click(function () { 
+    $('.bg-modal-fp').css('display', 'flex');
+    $('.bg-modal').css('display', 'none');
+});
+
+$('#login_create').click(function () { 
+    $('.bg-modal').css('display', 'flex');
+    $('.bg-modal-create').css('display', 'none');
+});
+
+$('#create_acc').click(function () { 
+    $('.bg-modal').css('display', 'none');
+    $('.bg-modal-create').css('display', 'flex');
+});
+
+$('#become_a_helper').click(function () { 
+    $('.bg-modal-become-helper').css('display', 'flex');
+});
+
+$('.getting_started').click(function () { 
+    $('.bg-modal-become-helper').css('display', 'none');
+});
+
+
+
+// Navbar Change 
+
+function changeBg(){
+    var scrollValue = window.scrollY;
+    // console.log(scrollValue);
+
+    var navbar = document.getElementById("header");
+    $('#header').css('background-color','transparent');
+
+    if(scrollValue > 80){
+        navbar.classList.add('bgcolor');
+        $('.logo img').css('height', '54px');
+        $('.logo img').css('width', '73px');
+        $('.nav_option ul li').css('margin', '10px 10px 0px');
+        $('#header').css('background-color','#525252');
+    }else{
+        navbar.classList.remove('bgcolor');
+        $('.logo img').css('height', '102px');
+        $('.logo img').css('width', '138px');
+        $('.nav_option ul li').css('margin', '43px 10px 0px');
+        $('#header').css('background-color','transparent');
+    }
+}
+
+window.addEventListener('scroll',changeBg);
+
+
+
+
+
+
+
 
 
 
